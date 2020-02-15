533e0bb98c6f4a92c88512f0b66cd6b35e599602
shimataro <shimataro@zelkova.cc>
2020-02-16 06:38:02 +0900

    * update examples

--- examples/example.mjs
+++ examples/example.mjs
@@ -93,7 +93,7 @@ const expected = { // should be converted to this
 };
 
 // Let's apply!
-const actual = vs.applySchemaObject(input, schemaObject);
+const actual = vs.applySchemaObject(schemaObject, input);
 
 // verification
 assert.deepStrictEqual(actual, expected);
