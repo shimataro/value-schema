055d9f30b6b5c9f90bbd477ba20f2bb1cdc71c4a
shimataro <shimataro@zelkova.cc>
2020-02-26 23:54:05 +0900

    * add converter option in number()

--- src/schemas/string.ts
+++ src/schemas/string.ts
@@ -1,8 +1,8 @@
+import * as converter from "../appliers/converter";
 import * as ifEmptyString from "../appliers/ifEmptyString";
 import * as ifNull from "../appliers/ifNull";
 import * as ifUndefined from "../appliers/ifUndefined";
 import * as only from "../appliers/only";
-import * as converter from "../appliers/converter";
 import * as maxLength from "../appliers/string/maxLength";
 import * as minLength from "../appliers/string/minLength";
 import * as pattern from "../appliers/string/pattern";
@@ -16,10 +16,10 @@ export const STRING = {
 };
 
 type OptionsForString =
+	converter.Options<string> |
 	ifEmptyString.Options<string> |
 	ifNull.Options<string> |
 	ifUndefined.Options<string> |
-	converter.Options<string> |
 	only.Options<string> |
 	type.Options |
 	trims.Options |
