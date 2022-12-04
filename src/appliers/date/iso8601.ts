f5b30ea4516be530fed17eac9daead1eec6c885e
shimataro <info@shimataro.me>
2022-12-04 22:18:43 +0900

    make seconds required

--- src/appliers/date/iso8601.ts
+++ src/appliers/date/iso8601.ts
@@ -1,8 +1,8 @@
 import {RULE, ValueSchemaError} from "../../exporter";
 import {Key, Values, isString} from "../../libs/types";
 
-const PATTERN_WITH_TZ = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}(:\d{2}(.\d{1,3})?)?(Z|[+-]\d{2}:\d{2})$/;
-const PATTERN_WITHOUT_TZ = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}(:\d{2}(.\d{1,3})?)?$/;
+const PATTERN_WITH_TZ = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(.\d{1,3})?(Z|[+-]\d{2}:\d{2})$/;
+const PATTERN_WITHOUT_TZ = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(.\d{1,3})?$/;
 
 export interface Rules
 {
